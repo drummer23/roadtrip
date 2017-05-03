3a56ac03a9f6002bc747857cce02e61ac6a5fbad
Roger Spielmann <drummer23@braveheart.ch>
2017-05-03 20:04:01 +0200

    adds image&styling to place-items

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
   return [
-    {name: 'Grand Canyon', state: 'Arizona'},
-    {name: 'Hoover Dam', state: 'Nevada'},
-    {name: 'Yosemite Park', state: 'California'}
+    {name: 'Grand Canyon', state: 'Arizona', image: '/assets/images/canyon.jpg'},
+    {name: 'Hoover Dam', state: 'Nevada', image: '/assets/images/hoover.jpg'},
+    {name: 'Yosemite Park', state: 'California', image: '/assets/images/yosemite.jpg'}
     ];
   }
 });
